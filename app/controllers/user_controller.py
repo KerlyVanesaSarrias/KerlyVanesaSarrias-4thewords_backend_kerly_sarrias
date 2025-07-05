cf3621aeae0b5dd2e74219e3a47d8bdb494f4520
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-07-04 20:36:31 -0500

    add authenticated user profile endpoint

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -5,6 +5,8 @@ from app.schemas.user_schema import UserCreate, UserResponse, UserLogin
 from app.services import user_service
 from app.core.security import create_access_token
 router = APIRouter(prefix="/auth", tags=["Auth"])
+from app.utils.auth_utils import get_current_user
+from app.models.user_model import User
 
 @router.post("/register", response_model=UserResponse)
 def register(data: UserCreate, session: Session = Depends(get_session)):
@@ -18,3 +20,7 @@ def login(data: UserLogin, session: Session = Depends(get_session)):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     token = create_access_token({"sub": user.email})
     return {"access_token": token, "token_type": "bearer"}
+
+@router.get("/me", response_model=UserResponse)
+def get_profile(current_user: User = Depends(get_current_user)):
+    return current_user
